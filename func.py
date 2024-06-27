6d8f0ccff64d140f5de6aba10c8dc4791852153c
Sagi Cohen <85191320+sagi778@users.noreply.github.com>
2024-06-28 00:51:02 +0300

    Add files via upload

--- func.py
+++ func.py
@@ -48,7 +48,59 @@ def load_config(file_path):
     except json.JSONDecodeError:
         print("Error parsing configuration file.")
         return {}  
+def read_data_file(file_full_path:str):
+    file_type = file_full_path.split('.')[-1] 
+    if file_type == 'csv':
+        return pd.read_csv(file_full_path)
+    else:
+        print('Unsupported file type.')
+        return pd.DataFrame()  
+def get_darker_color(hex_color, percentage=10):
+    """
+    Darkens the given hex color by the specified percentage.
+
+    :param hex_color: str, the hex color string (e.g., "#E8DAEF").
+    :param percentage: int, the percentage to darken the color (default is 10%).
+    :return: str, the darkened hex color string.
+    """
+    # Ensure the input is a valid hex color
+    if hex_color.startswith('#'):
+        hex_color = hex_color[1:]
+
+    # Convert hex to RGB
+    r = int(hex_color[0:2], 16)
+    g = int(hex_color[2:4], 16)
+    b = int(hex_color[4:6], 16)
+
+    # Darken the color by the specified percentage
+    factor = (100 - percentage) / 100
+    r = max(0, int(r * factor))
+    g = max(0, int(g * factor))
+    b = max(0, int(b * factor))
+
+    # Convert RGB back to hex
+    darkened_color = f"#{r:02x}{g:02x}{b:02x}"
+
+    return darkened_color
+
+# charts
+def get_scatter_plot(parent,df:pd.DataFrame,x:str=None,y:str=None):
+
+    # Create a CTkFrame to hold the chart
+    frame = ctk.CTkFrame(parent)
+
+    data = df[[x,y]].copy().dropna()
+    # Create a Seaborn plot
+    fig, ax = plt.subplots()
+    sns.scatterplot(data=df,x=x,y=y)
     
+    # Create a canvas to embed the plot
+    canvas = FigureCanvasTkAgg(fig, master=frame)
+    canvas.draw()
+    canvas.get_tk_widget().pack(fill="both", expand=True)
+    
+    return frame
+
 # data frame func
 def get_columns_info(df:pd.DataFrame): 
     #data = pd.DataFrame(columns=['column','type','Non-Nulls','Non-Nulls%'])
@@ -57,7 +109,7 @@ def get_columns_info(df:pd.DataFrame):
         data['column'].append(column)
         data['type'].append(str(df[column].dtype))
         data['unique'].append(len(df[column].unique().tolist()))
-        data['Non-Nulls'].append(len(df[column].isna()))
-        data['Non-Nulls%'].append(f"{round(len(df[column].isna())*100/len(df),2)}%")
+        data['Non-Nulls'].append(len(df[~df[column].isna()]))
+        data['Non-Nulls%'].append(f"{round(len(df[~df[column].isna()])*100/len(df),2)}%")
                          
-    return pd.DataFrame(data)    
+    return pd.DataFrame(data).sort_values(by=['Non-Nulls']).reset_index(drop=True)    
